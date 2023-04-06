002515536162335099293772fb52423b797f3968
DIDIK ADI DARMAWAN <109228277+lnhdad070917@users.noreply.github.com>
2023-04-06 16:48:08 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
-// import 'regenerator-runtime';
+import "./style/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-loader";
 
-import css from './style/style.css';
-import css from './style/style.scss';
-// Import our custom CSS
-// import '../src/style/style.scss';
-// // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
-import main from './script/main';
-
-main();
+(() => {
+  "use strict";
+  document
+    .querySelector(".search-button")
+    .addEventListener("click", async function () {
+      try {
+        const i = document.querySelector(".input-keyword");
+        !(function (t) {
+          let i = "";
+          t.forEach(
+            (t) =>
+              (i += (function (t) {
+                return `<div class="col-md-4 my-3">\n                <div class="card">\n                    <img class="card-img-top" src="${t.Poster}">\n                    <div class="card-body">\n                    <h5 class="card-title">${t.Title}</h5>\n                    <h6 class="card-subtitle mb-2 text-muted">${t.Year}</h6>\n                    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModalLabel" data-imdbid="${t.imdbID}">Show Details</a>\n                    </div>\n                </div>\n            </div>`;
+              })(t))
+          ),
+            (document.querySelector(".movie-container").innerHTML = i);
+        })(
+          await ((t = i.value),
+          fetch("http://www.omdbapi.com/?apikey=b692a87f&s=" + t)
+            .then((t) => {
+              if (!t.ok) throw new Error(t.statusText);
+              return t.json();
+            })
+            .then((t) => {
+              if ("False" === t.Response) throw new Error(t.Error);
+              return t.Search;
+            }))
+        );
+      } catch (t) {
+        alert(t);
+      }
+      var t;
+    }),
+    document.addEventListener("click", async function (t) {
+      t.target.classList.contains("modal-detail-button") &&
+        (t.target.dataset.imdbid,
+        (function (t) {
+          const i = (function (t) {
+            return `<div class="container-fluid">\n                <div class="row">\n                    <div class="col-md-3">\n                        <img src="${t.Poster}" class="img-fluid">\n                    </div>\n                    <div class="col-md">\n                    <ul class="list-group">\n                        <li class="list-group-item"><h4>${t.Title} ${t.Year}</h4></li>\n                        <li class="list-group-item"><strong>Director:</strong> ${t.Director}</li>\n                        <li class="list-group-item"><strong>Actors:</strong> ${t.Actors}</li>\n                        <li class="list-group-item"><strong>Writer:</strong> ${t.Writer}</li>\n                        <li class="list-group-item"><strong>Plot:</strong><br>${t.Plot}</li>\n                        </ul>\n                    </div>\n                </div>\n            </div>`;
+          })(t);
+          document.querySelector(".modal-body").innerHTML = i;
+        })(
+          await fetch("http://www.omdbapi.com/?apikey=b692a87f&i=" + id)
+            .then((t) => t.json())
+            .then((t) => t)
+        ));
+    });
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,80 @@
-const path = require('path')
-const autoprefixer = require('autoprefixer')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+  devtool: "source-map",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, "dist"),
     port: 8080,
-    hot: true
+    hot: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' })
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
   ],
   module: {
     rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: ["style-loader", "css-loader"],
+      // },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: "babel-loader",
+      //     options: {
+      //       presets: ["@babel/preset-env"],
+      //     },
+      //   },
+      // },
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     "style-loader",
+      //     // Translates CSS into CommonJS
+      //     "css-loader",
+      //     // Compiles Sass to CSS
+      //     "sass-loader",
+      //   ],
+      // },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          },
-        ]
+        use: ["style-loader", "css-loader"],
       },
+      // {
+      //   test: /\.scss$/,
+      //   use: ["style-loader", "css-loader", "sass-loader"],
+      // },
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+          loader: "babel-loader",
         },
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        // Loader for webpack to process CSS with PostCSS
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions: {
-            plugins: () => [
-              autoprefixer
-            ]
-          }
-        }
-      },
-      {
-        // Loads a SASS/SCSS file and compiles it to CSS
-        loader: 'sass-loader'
-      },
-    ]
+      // {
+      //   // Loader for webpack to process CSS with PostCSS
+      //   loader: "postcss-loader",
+      //   options: {
+      //     postcssOptions: {
+      //       plugins: () => [autoprefixer],
+      //     },
+      //   },
+      // },
+      // {
+      //   // Loads a SASS/SCSS file and compiles it to CSS
+      //   loader: "sass-loader",
+      // },
+    ],
   },
-  
-}
+};
