53e327e129fb66de4cca8ff6fb7829c321d8ef52
DIDIK ADI DARMAWAN <109228277+lnhdad070917@users.noreply.github.com>
2023-04-06 20:09:17 +0700

    ....

--- src/index.js
+++ src/index.js
@@ -1,54 +1,10 @@
+// Import CSS files
 import "./style/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import JS files
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-loader";
-
-// (() => {
-//   "use strict";
-//   document
-//     .querySelector(".search-button")
-//     .addEventListener("click", async function () {
-//       try {
-//         const i = document.querySelector(".input-keyword");
-//         !(function (t) {
-//           let i = "";
-//           t.forEach(
-//             (t) =>
-//               (i += (function (t) {
-//                 return `<div class="col-md-4 my-3">\n                <div class="card">\n                    <img class="card-img-top" src="${t.Poster}">\n                    <div class="card-body">\n                    <h5 class="card-title">${t.Title}</h5>\n                    <h6 class="card-subtitle mb-2 text-muted">${t.Year}</h6>\n                    <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetailModalLabel" data-imdbid="${t.imdbID}">Show Details</a>\n                    </div>\n                </div>\n            </div>`;
-//               })(t))
-//           ),
-//             (document.querySelector(".movie-container").innerHTML = i);
-//         })(
-//           await ((t = i.value),
-//           fetch("http://www.omdbapi.com/?apikey=b692a87f&s=" + t)
-//             .then((t) => {
-//               if (!t.ok) throw new Error(t.statusText);
-//               return t.json();
-//             })
-//             .then((t) => {
-//               if ("False" === t.Response) throw new Error(t.Error);
-//               return t.Search;
-//             }))
-//         );
-//       } catch (t) {
-//         alert(t);
-//       }
-//       var t;
-//     }),
-//     document.addEventListener("click", async function (event) {
-//       if (event.target.classList.contains("modal-detail-button")) {
-//         const imdbId = event.target.dataset.imdbid;
-//         const movieData = await fetch(
-//           `http://www.omdbapi.com/?apikey=b692a87f&i=${imdbId}`
-//         )
-//           .then((response) => response.json())
-//           .then((data) => data);
-//         const movieDetails = `<div class="container-fluid"> <div class="row"> <div class="col-md-3"> <img src="${movieData.Poster}" class="img-fluid"> </div> <div class="col-md"> <ul class="list-group"> <li class="list-group-item"><h4>${movieData.Title} ${movieData.Year}</h4></li> <li class="list-group-item"><strong>Director:</strong> ${movieData.Director}</li> <li class="list-group-item"><strong>Actors:</strong> ${movieData.Actors}</li> <li class="list-group-item"><strong>Writer:</strong> ${movieData.Writer}</li> <li class="list-group-item"><strong>Plot:</strong><br>${movieData.Plot}</li> </ul> </div> </div> </div>`;
-//         document.querySelector(".modal-body").innerHTML = movieDetails;
-//       }
-//     });
-// })();
 
 (() => {
   "use strict";
@@ -98,3 +54,9 @@ import "bootstrap-loader";
     }
   });
 })();
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+} else {
+  console.log("Looks like we are in production mode!");
+}
